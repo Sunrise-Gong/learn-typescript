e112f43625e1306b01305cbb486395fb54156ec7
Sunrise Gong <rhdgoehe2@gmail.com>
2021-04-30 22:07:25 +0900

    20210430[41.37%]

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,23 @@
+// interface Person {
+//     name: string;
+//     age: number;
+// };
+
+// 객체 타입 별칭
+type Person = {
+    name: string;
+    age: number;
+};
+
+var dozy: Person = {
+    name: '돋이',
+    age: 31,
+};
+
+// 문자열 타입별칭
+type Mystring = string;
+
+var str: Mystring = 'hi';
+
+
+

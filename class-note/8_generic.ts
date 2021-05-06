a44ba0bfe66a46970faf4b77c6568c1ede967e0e
Sunrise Gong <rhdgoehe2@gmail.com>
2021-05-06 23:40:41 +0900

    20210506[66.66%]

--- class-note/8_generic.ts
+++ class-note/8_generic.ts
@@ -16,3 +16,18 @@ logText_1(10)
 //function logText<string>(text: stirng):string
 const hello = logText_1<string>('hello');
 hello.split('');
+
+// 인터페이스에 제네릭을 선언하는 방법
+// // 기존 방식
+// interface Dropdown {
+//     value: string;
+//     selected: boolean;
+// }
+// const obj: Dropdown = { value: '안녕', selected: true };
+
+interface Dropdown<T> {
+    value: T;
+    selected: boolean;
+}
+const objStr: Dropdown<string> = { value: '안녕', selected: false };
+const objNum: Dropdown<number> = { value: 10, selected: true };

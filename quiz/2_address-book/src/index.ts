14492965ef7943591fdfaa9929bc4608001ff25b
Sunrise Gong <rhdgoehe2@gmail.com>
2021-05-10 22:02:31 +0900

    20210510[74.71%]

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -12,9 +12,9 @@ interface Contact {
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts() {
-  // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+function fetchContacts(): Promise<any> {
+  // TODO: 아래 변수의 타입을 지정해보세요.(Contact[])
+  const contacts: Contact[] = [
     {
       name: 'Tony',
       address: 'Malibu',
@@ -49,7 +49,7 @@ function fetchContacts() {
       },
     },
   ];
-  return new Promise(resolve => {
+  return new Promise(resolve => { // 이 리턴값의 타입은 무엇일까?
     setTimeout(() => resolve(contacts), 2000);
   });
 }
@@ -57,7 +57,7 @@ function fetchContacts() {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
   constructor() {
     this.fetchData();
@@ -70,21 +70,21 @@ class AddressBook {
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-  findContactByName(name) {
+  findContactByName(name: string) {
     return this.contacts.filter(contact => contact.name === name);
   }
 
-  findContactByAddress(address) {
+  findContactByAddress(address: string) {
     return this.contacts.filter(contact => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+  findContactByPhone(phoneNumber: number, phoneType: string) {
     return this.contacts.filter(
       contact => contact.phones[phoneType].num === phoneNumber
     );
   }
 
-  addContact(contact) {
+  addContact(contact: Contact) {
     this.contacts.push(contact);
   }
 

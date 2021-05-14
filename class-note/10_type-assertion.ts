9c1dc48005460c696b414228177fdfbc79f77446
Sunrise Gong <rhdgoehe2@gmail.com>
2021-05-14 22:00:31 +0900

    20210514[87.35%]

---
+++ class-note/10_type-assertion.ts
@@ -0,0 +1,7 @@
+// 타입 단언(type assertion)
+
+var c;
+c = 'hello'
+var d = c as string;
+
+// DOM API 를 조작할 때 타입단언을 많이 사용합니다.
--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -7,12 +7,34 @@ function getA(a = 10) {
 }
 
 // 타입 추론 기본 2
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+
+// var shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'hello'
+// }
+
+// 타입 추론 기본 3
 interface Dropdown<T> {
     value: T;
     title: string;
 }
 
-var shoppingItem: Dropdown<string> = {
-    value: 'hi',
-    title: 'hello'
+interface DetailedDropdown<K> extends Dropdown<K> { // 'convention' 하게는 <T>를 사용하지만 구분하기 위해서 <K>를 사용
+    description: string;
+    tag: K;
 }
+
+var shoppingItem: DetailedDropdown<number> = {
+    value: 1,
+    tag: 2,
+    description: 'value/tag 가 <K>로 추론',
+    title: '제네릭 인터페이스 상속'
+}
+
+
+// Best Common Type
+let arr = ['a', 2, true];
